0f761bb7530cc6b7eb2e0f0ec55fa27a92ff117f
Sagiri Izumi <romaty29@gmail.com>
2022-12-15 16:15:30 +0700

    d

---
+++ TaskCustomList/CustomList.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+
+namespace TaskCustomList;
+
+public class CustomList: IList
+{
+    private object _array = new object[10];
+    private int _count;
+    public CustomList()
+    {
+        _count = 0;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(Array array, int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsSynchronized { get; }
+    public object SyncRoot { get; }
+    
+    public int Add(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int IndexOf(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(object? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsFixedSize { get; }
+    public bool IsReadOnly { get; }
+
+    public object? this[int index]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+}
