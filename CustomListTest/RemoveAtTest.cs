b55a3518336af3fe3551049c8b42d49149368735
Sagiri Izumi <romaty29@gmail.com>
2022-12-19 15:19:38 +0700

    r

---
+++ CustomListTest/RemoveAtTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace CustomListTest;
+
+[TestClass]
+public class RemoveAtTest
+{
+    [TestMethod]
+    public void RemoveAt_ShouldReturnCorrectResult()
+    {
+        IList customList = new CustomList.CustomList{1,2,3,4,5};
+        
+        customList.RemoveAt(2);
+        var actual = customList[2];
+        const int expected = 4;
+        
+        Assert.AreEqual(expected, actual);
+    }
+}
