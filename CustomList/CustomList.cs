94838fe92da4c821c63f896c00039ba73278ad3d
Sagiri Izumi <romaty29@gmail.com>
2022-12-23 16:32:08 +0700

    end

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -1,22 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Collections;
+﻿using System.Collections;
 
 namespace CustomList;
 
-public class CustomList : IList
+public class CustomList<T> : IList, IList<T>
 {
-    private object?[] _array = Array.Empty<object?>();
+    //Ilist<T>
+    private T[] _array = Array.Empty<T>();
+    private object?[] _arrayObject = Array.Empty<object?>();
 
-    public int Add(object? value)
+    public void Add(T item)
     {
-        Insert(_array.Length, value);
-        return _array.Length - 1;
+        Insert(_array.Length, item);
     }
 
-    public void Insert(int index, object? value)
+    public void Insert(int index, T item)
     {
-        var newArray = new object?[_array.Length + 1];
-        newArray[index] = value;
+        var newArray = new T[_array.Length + 1];
+        newArray[index] = item;
         for (var i = 0; i < index; i++)
         {
             newArray[i] = _array[i];
@@ -30,11 +30,54 @@ public class CustomList : IList
         _array = newArray;
     }
 
-    public int IndexOf(object? value)
+    public void Clear()
+    {
+        var newArray = Array.Empty<T>();
+        _array = newArray;
+    }
+
+    public bool Contains(T item)
+    {
+        foreach (var a in _array)
+        {
+            if (Equals(a, item)) return true;
+        }
+
+        return false;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        foreach (var t in _array)
+        {
+            array.SetValue(t, arrayIndex);
+            arrayIndex++;
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        var newArray = new T[_array.Length - 1];
+        var index = IndexOf(item);
+
+        for (var i = index + 1; i < _array.Length; i++)
+        {
+            newArray[i - 1] = _array[i];
+        }
+
+        _array = newArray;
+
+        return index != -1;
+    }
+
+    public int Count => _array.Length;
+    public bool IsReadOnly => false;
+
+    public int IndexOf(T item)
     {
         for (var i = 0; i < _array.Length; i++)
         {
-            if (_array[i] == value)
+            if (Equals(_array[i], item))
             {
                 return i;
             }
@@ -43,54 +86,9 @@ public class CustomList : IList
         return -1;
     }
 
-    public IEnumerator GetEnumerator()
-    {
-        foreach (var t in _array)
-        {
-            yield return t;
-        }
-    }
-
-    public void CopyTo(Array array, int index)
-    {
-        foreach (var t in _array)
-        {
-            array.SetValue(t, index);
-            index++;
-        }
-    }
-
-    public int Count => _array.Length;
-    public bool IsSynchronized => false;
-    public object SyncRoot => null;
-
-    public void Clear()
-    {
-        var newArray = Array.Empty<object?>();
-        _array = newArray;
-    }
-
-    public bool Contains(object? value)
-    {
-        return _array.Any(a => a == value);
-    }
-
-    public void Remove(object? value)
-    {
-        var newArray = new object?[_array.Length - 1];
-        var index = IndexOf(value);
-
-        for (var i = index + 1; i < _array.Length; i++)
-        {
-            newArray[i - 1] = _array[i];
-        }
-
-        _array = newArray;
-    }
-
     public void RemoveAt(int index)
     {
-        var newArray = new object?[_array.Length - 1];
+        var newArray = new T[_array.Length - 1];
         for (var i = 0; i < index; i++)
         {
             newArray[i] = _array[i];
@@ -104,12 +102,96 @@ public class CustomList : IList
         _array = newArray;
     }
 
-    public bool IsFixedSize => true;
-    public bool IsReadOnly => false;
-
-    public object? this[int index]
+    public T this[int index]
     {
         get => _array[index];
         set => _array[index] = value;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var t in _array)
+        {
+            yield return t;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    //Ilist
+    public int Add(object? value)
+    {
+        Insert(_arrayObject.Length, value);
+        return _arrayObject.Length - 1;
+    }
+
+    public bool Contains(object? value)
+    {
+        return _arrayObject.Any(a => a == value);
+    }
+
+    public int IndexOf(object? value)
+    {
+        for (var i = 0; i < _arrayObject.Length; i++)
+        {
+            if (_arrayObject[i] == value)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void Insert(int index, object? value)
+    {
+        var newArray = new object?[_arrayObject.Length + 1];
+        newArray[index] = value;
+        for (var i = 0; i < index; i++)
+        {
+            newArray[i] = _arrayObject[i];
+        }
+
+        for (var i = index; i < _arrayObject.Length; i++)
+        {
+            newArray[i + 1] = _arrayObject[i];
+        }
+
+        _arrayObject = newArray;
+    }
+
+    public void Remove(object? value)
+    {
+        var newArray = new object?[_arrayObject.Length - 1];
+        var index = IndexOf(value);
+
+        for (var i = index + 1; i < _arrayObject.Length; i++)
+        {
+            newArray[i - 1] = _arrayObject[i];
+        }
+
+        _arrayObject = newArray;
+    }
+
+    public void CopyTo(Array array, int index)
+    {
+        foreach (var t in _arrayObject)
+        {
+            array.SetValue(t, index);
+            index++;
+        }
+    }
+
+    public bool IsSynchronized => false;
+    public object SyncRoot => false;
+    public bool IsFixedSize => true;
+
+    object? IList.this[int index]
+    {
+        get => _arrayObject[index];
+        set => _arrayObject[index] = value;
     }
 }
--- CustomListTest/CustomListStringTest.cs
+++ CustomListTest/CustomListStringTest.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CustomList;
 
 namespace CustomListTest;
@@ -5,7 +6,16 @@ namespace CustomListTest;
 [TestClass]
 public class CustomListStringTest
 {
-    IList<string> customList = new CustomList<string>
+    private readonly IList<string> _customList = new CustomList<string>
+    {
+        "Oliver",
+        "Jack",
+        "Harry",
+        "Jacob",
+        "Charley"
+    };
+
+    private readonly IList _customListObj = new CustomList<object>
     {
         "Oliver",
         "Jack",
@@ -17,8 +27,8 @@ public class CustomListStringTest
     [TestMethod]
     public void Add_ShouldReturnCorrectResult()
     {
-        customList.Add("Thomas");
-        var actual = customList.Count;
+        _customList.Add("Thomas");
+        var actual = _customList.Count;
         const int expected = 6;
 
         Assert.AreEqual(expected, actual);
@@ -27,8 +37,8 @@ public class CustomListStringTest
     [TestMethod]
     public void Insert_ShouldReturnCorrectResult()
     {
-        customList.Insert(2, "Thomas");
-        var actual = customList[2];
+        _customList.Insert(2, "Thomas");
+        var actual = _customList[2];
         const string expected = "Thomas";
 
         Assert.AreEqual(expected, actual);
@@ -37,8 +47,8 @@ public class CustomListStringTest
     [TestMethod]
     public void Clear_ShouldReturnCorrectResult()
     {
-        customList.Clear();
-        var actual = customList.Count;
+        _customList.Clear();
+        var actual = _customList.Count;
         const int expected = 0;
 
         Assert.AreEqual(expected, actual);
@@ -47,7 +57,7 @@ public class CustomListStringTest
     [TestMethod]
     public void Contains_ShouldReturnCorrectResult()
     {
-        var actual = customList.Contains("Oliver");
+        var actual = _customList.Contains("Oliver");
         const bool expected = true;
 
         Assert.AreEqual(expected, actual);
@@ -56,7 +66,7 @@ public class CustomListStringTest
     [TestMethod]
     public void IndexOf_ShouldReturnCorrectResult()
     {
-        var actual = customList.IndexOf("Oliver");
+        var actual = _customList.IndexOf("Oliver");
         const int expected = 0;
 
         Assert.AreEqual(expected, actual);
@@ -65,8 +75,8 @@ public class CustomListStringTest
     [TestMethod]
     public void RemoveAt_ShouldReturnCorrectResult()
     {
-        customList.RemoveAt(2);
-        var actual = customList[2];
+        _customList.RemoveAt(2);
+        var actual = _customList[2];
         const string expected = "Jacob";
 
         Assert.AreEqual(expected, actual);
@@ -75,8 +85,8 @@ public class CustomListStringTest
     [TestMethod]
     public void Remove_ShouldReturnCorrectResult()
     {
-        customList.Remove("Jacob");
-        var actual = customList[3];
+        _customList.Remove("Jacob");
+        var actual = _customList[3];
         const string expected = "Charley";
 
         Assert.AreEqual(expected, actual);
@@ -87,7 +97,66 @@ public class CustomListStringTest
     {
         var copyArray = new string[5];
 
-        customList.CopyTo(copyArray, 0);
+        _customList.CopyTo(copyArray, 0);
+        var actual = copyArray[0];
+        const string expected = "Oliver";
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void AddObj_ShouldReturnCorrectResult()
+    {
+        var actual = _customListObj.Add("Thomas");
+        const int expected = 5;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void InsertObj_ShouldReturnCorrectResult()
+    {
+        _customListObj.Insert(2, "Thomas");
+        var actual = _customListObj[2];
+        const string expected = "Thomas";
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void ContainsObj_ShouldReturnCorrectResult()
+    {
+        var actual = _customListObj.Contains("Oliver");
+        const bool expected = true;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void IndexOfObj_ShouldReturnCorrectResult()
+    {
+        var actual = _customListObj.IndexOf("Oliver");
+        const int expected = 0;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void RemoveObj_ShouldReturnCorrectResult()
+    {
+        _customListObj.Remove("Jacob");
+        var actual = _customListObj[3];
+        const string expected = "Charley";
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void CopyToObj_ShouldReturnCorrectResult()
+    {
+        var copyArray = new string[5];
+
+        _customListObj.CopyTo(copyArray, 0);
         var actual = copyArray[0];
         const string expected = "Oliver";
 
